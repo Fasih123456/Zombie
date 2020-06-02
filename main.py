1c5b85b9f9badb80b1a19bbf0263dacfb924d7ae
Fasih123456 <fasihislam1999@gmail.com>
2020-06-02 20:33:36 -0300

    Made menu functionality added

--- main.py
+++ main.py
@@ -7,23 +7,58 @@ from pygame import mixer
 pygame.init()
 
 # create the screen of 800x600
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 
 # Main Menu
 main_menu_font = pygame.font.Font("freesansbold.ttf", 32)
 
 
 def mainmenu():
-    heading = main_menu_font.render("Welcome to war games", True, (255, 255, 255))
-    screen.blit(heading, (300, 250))
+    heading = main_menu_font.render("Welcome to Zombie War", True, (255, 255, 255))
+    screen.blit(heading, (210, 10))
 
+    play = main_menu_font.render("Play", True, (255, 0, 0))
+    screen.blit(play, (340, 125))
+
+def button (msg,x,y,w,h,i,a):
+    mouse = pygame.mouse.get_pos()
 
 # Main program
-mainmenu()
+isDoneMenu = True
 isDone = True
-while isDone:
+while isDoneMenu:
+    screen.fill((0, 0, 0))
+
+    print("Running")
+
+    # buttons
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if 325 + 100 > mouse[0] > 325 and 125 + 50 > mouse[1] > 125:
+        pygame.draw.rect(screen, (0, 255, 0), (325, 115, 100, 50))
+        if click[0] == 1:
+            isDoneMenu = False
+    else:
+        pygame.draw.rect(screen, (0, 200, 0), (325, 115, 100, 50))
+
     mainmenu()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            isDoneMenu = False
             isDone = False
+
+    pygame.display.update()
+
+
+while isDone:
+    screen.fill((0, 0, 0))
+    print("running2")
+
+    #Pressing keys does stuff
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isDone = False
+
+    pygame.display.update()
